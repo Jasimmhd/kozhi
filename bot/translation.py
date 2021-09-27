94eb6fcd007118ccaffee6c41b08abccadff40f4
Jasim <89487918+Jasimmhd@users.noreply.github.com>
2021-09-27 20:14:24 +0530

    Initial commit

---
+++ bot/translation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>    Don't ask film here❗
+       Im assistant manager of Flim zone group
+       You can ask them Films in the Group🎦
+                      
+                         Thankyou 😊
+</i>"""    
+     
+    HELP_TEXT = """
+<i> Hi how are you? 🥰
+</i>"""
+
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    
+<b>➥ Creator</b> : <b>@jasimmhd</b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : </b>@filmzone0</b>
+
+"""
